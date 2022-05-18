26d1018f2fb385ba9f76649d3d3d87f87c88260f
Inspiaaa <lavaafterburner@gmail.com>
2022-05-18 23:41:49 +0200

    Fix pause / resume behaviour of Music

--- src/game/sound/Music.java
+++ src/game/sound/Music.java
@@ -3,6 +3,8 @@ package game.sound;
 import javax.sound.sampled.*;
 import java.io.File;
 
+// The Music class represents sound data that is so large that it should
+// be streamed from the disk instead of loading all the data upfront.
 public class Music implements Runnable {
     private final int BUFFER_SIZE = 64 * 1024;
 
@@ -10,7 +12,8 @@ public class Music implements Runnable {
     private SourceDataLine soundLine;
     private AudioInputStream audioInputStream;
 
-    private volatile boolean isPlaying;
+    private volatile boolean isStreamingData;
+    private Thread dataStreamingThread;
 
     public Music(String filepath) {
         this.filepath = filepath;
@@ -21,6 +24,8 @@ public class Music implements Runnable {
             DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
             soundLine = (SourceDataLine) AudioSystem.getLine(info);
             soundLine.open(audioFormat);
+
+            audioInputStream.mark(Integer.MAX_VALUE);
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -29,46 +34,69 @@ public class Music implements Runnable {
 
     @Override
     public void run() {
-        soundLine.start();
-
         int numBytesRead = 0;
         byte[] sampledData = new byte[BUFFER_SIZE];
 
         try {
-            while (numBytesRead != -1 && isPlaying) {
+            while (numBytesRead != -1 && isStreamingData) {
                 numBytesRead = audioInputStream.read(sampledData, 0, sampledData.length);
                 if (numBytesRead >= 0) {
                     soundLine.write(sampledData, 0, numBytesRead);
                 }
+                System.out.println(numBytesRead);
             }
         }
         catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
-        soundLine.drain();
-        soundLine.close();
+    private void startStreamingData() {
+        stopStreamingData();
+
+        isStreamingData = true;
+        dataStreamingThread = new Thread(this);
+        dataStreamingThread.start();
+    }
+
+    private void stopStreamingData() {
+        isStreamingData = false;
+
+        if (dataStreamingThread != null) {
+            try {
+                dataStreamingThread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void play() {
-        // TODO: soundLine.flush()
-        isPlaying = true;
-        Thread t = new Thread(this);
-        t.start();
+        try {
+            audioInputStream.reset();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // When the music was paused the last time and then restarted, the buffer may still have some content.
+        // This flushes the buffer so that it will play from the beginning again.
+        soundLine.flush();
+        soundLine.start();
+
+        startStreamingData();
     }
 
     public void pause() {
-        // isPlaying = false;
+        System.out.println("Pause");
+        stopStreamingData();
         soundLine.stop();
     }
 
     public void resume() {
+        System.out.println("Resume");
         soundLine.start();
-    }
-
-    public void stop() {
-        isPlaying = false;
-        soundLine.stop();
+        startStreamingData();
     }
 
     public boolean isPlaying() {
