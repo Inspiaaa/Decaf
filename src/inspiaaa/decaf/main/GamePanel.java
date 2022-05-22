d84c62f525b0e369f173514267f10786a297772c
Inspiaaa <lavaafterburner@gmail.com>
2022-05-22 15:31:32 +0200

    Decouple game loop rendering from repaint

--- src/inspiaaa/decaf/main/GamePanel.java
+++ src/inspiaaa/decaf/main/GamePanel.java
@@ -3,15 +3,21 @@ package inspiaaa.decaf.main;
 import inspiaaa.decaf.Time;
 import inspiaaa.decaf.input.Keyboard;
 import inspiaaa.decaf.input.Mouse;
+import inspiaaa.decaf.rendering.GraphicsHelper;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class GamePanel extends JPanel implements Runnable {
     private Thread gameThread;
     private boolean isRunning;
 
     private SceneManager sceneManager;
+    private volatile BufferedImage backBuffer;
+    private final Lock backBufferLock = new ReentrantLock();
 
     private float maxFps = 60;
 
@@ -30,10 +36,22 @@ public class GamePanel extends JPanel implements Runnable {
     public void startGame() {
         sceneManager = SceneManager.getInstance();
         sceneManager.start(getWidth(), getHeight());
+        resizeBackBuffer();
+
         isRunning = true;
 
         gameThread = new Thread(this);
         gameThread.start();
+    }
+
+    private void resizeBackBuffer() {
+        backBufferLock.lock();
+        try {
+            backBuffer = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
+        }
+        finally {
+            backBufferLock.unlock();
+        }
     }
 
     public void run() {
@@ -50,6 +68,7 @@ public class GamePanel extends JPanel implements Runnable {
             // synchronisation hassles between the UI thread and the game thread.
             if (newWidth != width || newHeight != height) {
                 sceneManager.onResize(newWidth, newHeight);
+                resizeBackBuffer();
                 width = newWidth;
                 height = newHeight;
             }
@@ -57,11 +76,22 @@ public class GamePanel extends JPanel implements Runnable {
             timeManager.update();
 
             try {
-                repaint();
+                sceneManager.update();
             }
             catch (Exception e) {
                 e.printStackTrace();
             }
+
+            // paintComponent() isn't actually called directly when repaint() is called.
+            // Probably also runs in multiple different threads.
+            backBufferLock.lock();
+            try {
+                backBuffer.getGraphics().drawImage(sceneManager.getScreen(), 0, 0, null);
+            }
+            finally {
+                backBufferLock.unlock();
+            }
+            repaint();
 
             timeManager.capFps(maxFps);
         }
@@ -71,8 +101,14 @@ public class GamePanel extends JPanel implements Runnable {
     protected void paintComponent(Graphics g) {
         if (isRunning) {
             super.paintComponent(g);
-            sceneManager.update();
-            g.drawImage(sceneManager.getScreen(), 0, 0, null);
+
+            backBufferLock.lock();
+            try {
+                g.drawImage(backBuffer, 0, 0, null);
+            }
+            finally {
+                backBufferLock.unlock();
+            }
         }
     }
 }
