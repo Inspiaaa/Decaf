f410bafc3957e912fe8ced3dbdf13c576c928d30
Inspiaaa <lavaafterburner@gmail.com>
2022-05-26 19:40:25 +0200

    Add listener system to Transform for position

---
+++ src/inspiaaa/decaf/IPositionListener.java
@@ -0,0 +1,7 @@
+package inspiaaa.decaf;
+
+import inspiaaa.decaf.maths.Vector2;
+
+public interface IPositionListener {
+    public void onPositionChange(Vector2 newPos);
+}
--- src/inspiaaa/decaf/Transform.java
+++ src/inspiaaa/decaf/Transform.java
@@ -3,10 +3,15 @@ package inspiaaa.decaf;
 import inspiaaa.decaf.gameobject.Component;
 import inspiaaa.decaf.maths.Vector2;
 
+import java.util.ArrayList;
+
 public class Transform extends Component {
     private Vector2 position;
     private Vector2 scale;
     private float rotation;
+
+    // Lazy initialised
+    private ArrayList<IPositionListener> positionListeners;
 
     public Transform() {
         this(new Vector2(0, 0));
@@ -23,7 +28,11 @@ public class Transform extends Component {
     }
 
     public void setPosition(Vector2 position) {
+        if (this.position.equals(position)) {
+            return;
+        }
         this.position = position;
+        informPositionListeners();
     }
 
     public float getRotation() {
@@ -40,5 +49,28 @@ public class Transform extends Component {
 
     public void setScale(Vector2 scale) {
         this.scale = scale;
+    }
+
+    private void informPositionListeners() {
+        if (positionListeners == null) {
+            return;
+        }
+
+        for (IPositionListener listener : positionListeners) {
+            listener.onPositionChange(position);
+        }
+    }
+
+    public void addPositionListener(IPositionListener listener) {
+        if (positionListeners == null) {
+            positionListeners = new ArrayList<IPositionListener>();
+        }
+        positionListeners.add(listener);
+    }
+
+    public void removePositionListener(IPositionListener listener) {
+        if (positionListeners != null) {
+            positionListeners.remove(listener);
+        }
     }
 }
